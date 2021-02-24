d6ba5a57d11ed38027d0bcb1524b65c32adf8001
Karel <karel@manjaro.home>
2021-02-24 18:25:50 +0100

    Measurements

---
+++ enable_dynamic_freq_scaling.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# http://nicolas.limare.net/pro/notes/2014/11/26_ht_freq_scal/
+
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo ondemand > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <bits/stdc++.h> 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "rapl-powercap.h"
 // #include "measure-no-unrolling.h"
@@ -22,32 +24,45 @@ using namespace std;
 
 void write_array_to_file(string& filename, long* measurements, size_t number_of_measurements);
 
+
 extern "C" void instruction_nop_100();
 extern "C" void instruction_nop_1000000();
-/*
-extern "C" void instruction_xor_100();
-extern "C" void instruction_xor_1000000();
-extern "C" void instruction_fscale_100();
-extern "C" void instruction_fscale_1000000();
-extern "C" void instruction_inc_100();
-extern "C" void instruction_inc_1000000();
-extern "C" void instruction_movl_100();
-extern "C" void instruction_movl_1000000();
-extern "C" void instruction_rdrand_100();
-extern "C" void instruction_rdrand_1000000();
-*/
+
+// extern "C" void instruction_xor_100();
+// extern "C" void instruction_xor_1000000();
+
+// extern "C" void instruction_fscale_100();
+// extern "C" void instruction_fscale_1000000();
+
+// extern "C" void instruction_inc_100();
+// extern "C" void instruction_inc_1000000();
+
+// extern "C" void instruction_movl_100();
+// extern "C" void instruction_movl_1000000();
+
+// extern "C" void instruction_rdrand_100();
+// extern "C" void instruction_rdrand_1000000();
+
 
 int main() {
 
-    cout<<"start main"<<endl;
-    instruction_xor_100();
-    long start = get_energy_counter();
-    instruction_xor_1000000();
-    instruction_xor_1000000();
-    instruction_xor_1000000();
-    long end = get_energy_counter();
+    system("/bin/bash -c ./disable_dynamic_freq_scaling.sh");
 
-    cout<<start<<" => "<<end<<":"<<end-start<<endl;
+    int number_of_measurements = 10000;
+    long measurements[number_of_measurements];
+    for(int i=0;i<number_of_measurements;i++) {
+        // filling up the pipeline
+        instruction_nop_100();
+        long start = get_energy_counter();
+        instruction_nop_1000000();
+        long stop = get_energy_counter();
+        measurements[i] = (stop-start);
+    }
+
+    string filename = "nop.csv";
+    write_array_to_file(filename, measurements, (size_t)number_of_measurements);
+
+    system("/bin/bash -c ./enable_dynamic_freq_scaling.sh");
 
 	return 0;
 }
